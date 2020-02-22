e5ff1db327ae4c4412daead44a05e6f36fc90b1f
andresqb198 <andresqb198@gmail.com>
2020-02-22 15:02:51 -0500

    Problem 1 solved

--- src/ProjectE/ProjectEuler.java
+++ src/ProjectE/ProjectEuler.java
@@ -1,10 +1,12 @@
 package ProjectE;
+import algorithmsEuler.*;
 
 public class ProjectEuler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("a");
+			Problems problemas = new Problems();
+			System.out.println("Problema Uno: " + problemas.problemOne());
 	}
 
 }
--- src/algorithmsEuler/Problems.java
+++ src/algorithmsEuler/Problems.java
@@ -1,5 +1,10 @@
-package Algoritms;
+package algorithmsEuler;
+import Math.Mathematics;
 
 public class Problems {
 
+	public int problemOne() {
+		Mathematics problem = new Mathematics();
+		return problem.sumDivBy(5, 999) + problem.sumDivBy(3, 999) -problem.sumDivBy(15, 999);
+	}
 }
