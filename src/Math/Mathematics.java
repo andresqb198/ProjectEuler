bdadf8ba46228eb1499d66af60fd5bfff45d1b65
andresqb198 <andresqb198@gmail.com>
2020-02-23 20:37:05 -0500

    Problem 2 solved

--- src/Math/Mathematics.java
+++ src/Math/Mathematics.java
@@ -1,10 +1,15 @@
 package Math;
+import java.lang.Math;
 
 public class Mathematics {
 	
 	public int sumDivBy(int n, int target) {
 		int p = target/n;
 		return n*(p*(p+1))/ 2;		
+	}
+	
+	public int generatorFibonacci(double n) {		
+		return (int)(((1/Math.sqrt(5))*Math.pow(((1+Math.sqrt(5))/2), n))+0.5);
 	}
 	
 }
