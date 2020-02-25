5d50fc707481d39540ce8c7e6b9994d908a43929
andresqb198 <andresqb198@gmail.com>
2020-02-24 20:14:12 -0500

    Problem 3 solved

--- src/ProjectE/ProjectEuler.java
+++ src/ProjectE/ProjectEuler.java
@@ -7,8 +7,9 @@ public class ProjectEuler {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Problems problemas = new Problems();
-			Mathematics math = new Mathematics();			
-			System.out.println("Problema dos: " + problemas.problemTwo());
+			Mathematics math = new Mathematics();	
+			
+			System.out.println("Problema 3: " + problemas.problemThree());
 	}
 
 }
